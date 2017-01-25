9aee538a3637d78e56e655ee4f8b6d1f077f9b24
Chih-Chen Kao <elros.morlin@gmail.com>
2017-01-26 01:11:31 +0800

    add build config
    Signed-off-by: Chih-Chen Kao <elros.morlin@gmail.com>

---
+++ premake5.lua
@@ -0,0 +1,45 @@
+
+
+workspace "StreamingPrototype"
+
+    location "Generated"
+
+    language "C++"
+
+    configurations {"Debug", "Release"}
+    platforms {"x64", "x32"}
+
+    filter { "platforms:x64" }
+        architecture "x86_64"
+    filter { "platforms:x32" }
+        architecture "x86"
+    
+    filter { }
+
+
+    filter { "configurations:Debug" }
+        flags { "Symbols" }
+
+    filter { "configurations:Release" }
+        -- Release should be optimized
+        optimize "On"
+
+    -- Reset the filter for other settings
+    filter { }
+
+
+    targetdir ("Build/Bin/%{prj.name}/%{cfg.buildcfg}/%{cfg.platform}")
+    objdir ("Build/Obj/%{prj.name}/%{cfg.buildcfg}/%{cfg.platform}")
+
+
+
+    -- find include dirs and link with Boost
+    dofile {"./FindBoost.lua"}
+    
+    project "SP"
+
+        kind "ConsoleApp"
+
+        includedirs {"SP/include"}
+        files {"SP/**.cpp"}
+
