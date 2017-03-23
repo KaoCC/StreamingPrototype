28714cdd7a13a92dbce13bd1aed73c727aba2bbb
Chih-Chen Kao <elros.morlin@gmail.com>
2017-03-23 19:41:02 +0800

    Camera functions
    set cemera functions

--- SP/src/Renderer/Scene/Camera.cpp
+++ SP/src/Renderer/Scene/Camera.cpp
@@ -10,12 +10,45 @@ namespace SP {
 		upVec = cross(rightVec, forwardVec);
 	}
 
+	void PerspectiveCamera::setFocusDistance(float distance) {
+		focusDistance = distance;
+		setDirty(true);
+	}
+
+	float PerspectiveCamera::getFocusDistance() const {
+		return focusDistance;
+	}
+
+	void PerspectiveCamera::setAperture(float aper) {
+		aperture = aper;
+		setDirty(true);
+	}
+
+	float PerspectiveCamera::getAperture() const {
+		return aperture;
+	}
+
+	void PerspectiveCamera::setSensorSize(RadeonRays::float2 const & size) {
+		dim = size;
+		setDirty(true);
+	}
+
 	RadeonRays::float2 PerspectiveCamera::getSensorSize() const {
 		return dim;
 	}
 
+	void PerspectiveCamera::setFocalLength(float length) {
+		focalLength = length;
+		setDirty(true);
+	}
+
 	float PerspectiveCamera::getFocalLength() const {
 		return focalLength;
+	}
+
+	void PerspectiveCamera::setDepthRange(RadeonRays::float2 const & range) {
+		zcap = range;
+		setDirty(true);
 	}
 
 	RadeonRays::float2 PerspectiveCamera::getDepthRange() const {
@@ -36,6 +69,10 @@ namespace SP {
 
 	RadeonRays::float3 PerspectiveCamera::getPosition() const {
 		return position;
+	}
+
+	float PerspectiveCamera::getAspectRatio() const {
+		return aspect;
 	}
 
 }
--- SP/src/Renderer/Scene/Camera.hpp
+++ SP/src/Renderer/Scene/Camera.hpp
@@ -21,6 +21,11 @@ namespace SP {
 		// camera position, camera look at, up vector
 		PerspectiveCamera(RadeonRays::float3 const& eye, RadeonRays::float3 const& at, RadeonRays::float3 const& up);
 
+		void setFocusDistance(float distance);
+		float getFocusDistance() const;
+
+		void setAperture(float aperture);
+		float getAperture() const;
 
 		void setSensorSize(RadeonRays::float2 const& size);
 		RadeonRays::float2 getSensorSize() const;
@@ -28,18 +33,14 @@ namespace SP {
 		void setFocalLength(float length);
 		float getFocalLength() const;
 
-
-
 		void setDepthRange(RadeonRays::float2 const& range);
 		RadeonRays::float2 getDepthRange() const;
-
 
 		RadeonRays::float3 getForwardVector() const;
 		RadeonRays::float3 getRightVector() const;
 		RadeonRays::float3 getUpVector() const;
 		RadeonRays::float3 getPosition() const;
 		float getAspectRatio() const;
-
 
 
 		// trans
@@ -63,6 +64,9 @@ namespace SP {
 		RadeonRays::float2 zcap;
 
 		float focalLength;
+		float aspect;
+		float focusDistance;
+		float aperture;
 
 	};
 
