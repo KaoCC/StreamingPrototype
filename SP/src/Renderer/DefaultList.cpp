1dec8db21abf562dab4b8ee034b28018ce1629da
Chih-Chen Kao <elros.morlin@gmail.com>
2017-10-10 17:45:01 +0800

    implement square in the default list
    implement square in the default list

--- SP/src/Renderer/DefaultList.cpp
+++ SP/src/Renderer/DefaultList.cpp
@@ -4,6 +4,10 @@
 #include "Scene/Material.hpp"
 
 
+// test
+
+#include <iostream>
+
 SP::Triangle::Triangle() {
 
 	RadeonRays::float4 vertices[] {
@@ -45,17 +49,77 @@ SP::Triangle::Triangle() {
 
 	// memory leak !
 	Material* diffuse = new SingleBxDF(SingleBxDF::BxDFType::kLambert);
-	diffuse->setInputValue("albedo", RadeonRays::float3(10, 10, 10));
+	diffuse->setInputValue("albedo", RadeonRays::float3(10, 15, 10));
 
 	setMaterial(diffuse);
 
 }
 
+
+SP::Square::Square() {
+
+	// test
+	std::cerr << "Square !!!! " << std::endl;
+
+	RadeonRays::float4 vertices[] {
+		{ 0.f, 0.f, 0.f, 1.0f },
+		{ 0.f, 1.f, 0.f, 1.0f },
+		{ 1.f, 0.f, 0.f, 1.0f },
+		{ 1.f, 1.f, 0.f, 1.0f }
+	};
+
+	const unsigned int unitLength = 1;
+
+	const size_t numOfVertices = 4;
+
+	uint32_t indices[] { 0, 1, 2, 1, 2, 3 };
+	const size_t numOfIndices = 6;
+
+	float normals[] {
+		1, 0, 0,
+		1, 0, 0,
+		1, 0, 0,
+		1, 0, 0
+	};
+	const size_t numOfNormals = 4;
+
+
+	// set vertices
+	setVertices(vertices, numOfVertices);
+
+	// set indices
+	setIndices(indices, numOfIndices);
+
+	// set Normals
+	setNormals(normals, numOfNormals);
+
+	// Generate Zeros if we do not have UVs
+	std::vector<RadeonRays::float2> zero(numOfVertices);
+	std::fill(zero.begin(), zero.end(), RadeonRays::float2(0, 0));
+	setUVs(zero.data(), numOfVertices);
+
+	setName("DefaultSquare");
+
+
+	// memory leak !
+	Material* diffuse = new SingleBxDF(SingleBxDF::BxDFType::kLambert);
+	diffuse->setInputValue("albedo", RadeonRays::float3(15, 10, 10));
+
+	setMaterial(diffuse);
+
+}
+
+
+
 SP::Mesh* SP::createDefaultShape(DefaultShapeType type) {
+
 	switch (type) {
 
 	case DefaultShapeType::kTriangle:
 		return new Triangle();
+		break;
+	case DefaultShapeType::kSquare:
+		return new Square();
 		break;
 
 	default:
@@ -64,3 +128,5 @@ SP::Mesh* SP::createDefaultShape(DefaultShapeType type) {
 		break;
 	}
 }
+
+
